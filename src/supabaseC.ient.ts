e25e41a1e59ccf333d6400d86162ba1898d3b5bb
magaines121-hash <magaines121@gmail.com>
2025-08-16 00:33:05 -0400

    Create supabaseC.ient.ts

---
+++ src/supabaseC.ient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+// Create a single Supabase client for the whole app
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
